11d8b768acc3dc9e512706645bbbf04453e0f712
Srajaltripathi <138282019+stevejobs1524@users.noreply.github.com>
2025-07-14 01:32:50 +0530

    Update data_loader.py
    Added data loader Script

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,26 @@
+# src/data_loader.py
+
+import pandas as pd
+import os
+
+DATA_PATH = "data"
+
+def load_csv(filename):
+    path = os.path.join(DATA_PATH, filename)
+    return pd.read_csv(path)
+
+def load_all_data():
+    races = load_csv("races.csv")
+    results = load_csv("results.csv")
+    drivers = load_csv("drivers.csv")
+    constructors = load_csv("constructors.csv")
+    pit_stops = load_csv("pit_stops.csv")
+
+    return {
+        "races": races,
+        "results": results,
+        "drivers": drivers,
+        "constructors": constructors,
+        "pit_stops": pit_stops
+    }
 
