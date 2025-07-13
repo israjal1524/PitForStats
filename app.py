45ee052eee54b30f94cdc84b95b37cc11e650443
Srajaltripathi <138282019+stevejobs1524@users.noreply.github.com>
2025-07-14 01:33:42 +0530

    Update app.py
    Just testiong data loader

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from src.data_loader import load_all_data
+
+st.title("🏎️ PitForStats – Data Load Test")
+
+data = load_all_data()
+st.write("🧠 Previewing datasets:")
+
+for name, df in data.items():
+    st.subheader(name.upper())
+    st.dataframe(df.head(5))
 
