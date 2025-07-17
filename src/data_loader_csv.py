38633a3e6727b2c37b88cf3a0503239b41528f85
Srajaltripathi <138282019+israjal1524@users.noreply.github.com>
2025-07-18 00:40:20 +0530

    Update data_loader_csv.py
    for Version Control

--- src/data_loader_csv.py
+++ src/data_loader_csv.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+def load_csv(filename):
+    return pd.read_csv(f"data/{filename}")
+
+def load_all_data():
+    return {
+        "races": load_csv("races.csv"),
+        "results": load_csv("results.csv"),
+        "drivers": load_csv("drivers.csv"),
+        "constructors": load_csv("constructors.csv"),
+    }
